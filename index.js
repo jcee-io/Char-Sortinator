7b81c9a933a29934929e0f69f5018406aee5ce51
Justin <jvcruz@ucdavis.edu>
2018-01-22 14:53:15 -0800

    got form to work, as well as post requests

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
+let myString = 'hello world';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.render('client');
+	res.render('client', { myString });
 });
 
+app.post('/', (req,res) => {
+	let string = req.body.string;
+	let arr = [...string].sort();
+  
+	myString = arr.join('');
+  res.render('client', { myString });
+});
 app.listen(process.env.PORT || 3000);
