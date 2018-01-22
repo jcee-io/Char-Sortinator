4dba3c4b5535a5aefab15528acf4548698125042
Justin <jvcruz@ucdavis.edu>
2018-01-22 14:25:35 -0800

    made views folder for ejs, created form and click handler

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('client.html'));
-
 app.get('/', (req, res) => {
 	res.render('client');
 });
---
+++ views/client.js
@@ -0,0 +1,3 @@
+const clickHandler = event => {
+  console.log(e);
+};
