650b25ada1e36375961e7fbc0287223c643200ef
Justin <jvcruz@ucdavis.edu>
2018-01-22 14:13:49 -0800

    converted html to ejs file, installed ejs

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 
 const app = express();
+
+app.set('view engine', 'ejs');
 app.use(express.static('client.html'));
 
 app.get('/', (req, res) => {
-	res.send('hello world');
+	res.render('client');
 });
 
 app.listen(process.env.PORT || 3000);
